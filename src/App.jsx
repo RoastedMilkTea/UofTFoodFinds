bea3e54d36596c3ccc5952a6a0a220a0e5f13b47
xlgraceli <xlgraceli@gmail.com>
2024-07-17 00:34:26 -0400

    added the bg, attempting to make it translucent

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/navigationbar/Navbar'
 import Campus from './components/Campus/Campus'
+import './App.css'
 
 const App = () => {
   return (
