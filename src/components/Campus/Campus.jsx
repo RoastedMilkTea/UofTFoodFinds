aaca18c46a1592f31956efcd0d10f5dc454e7714
JessieZ <jessie.zhouu3146@gmail.com>
2024-07-20 22:21:02 -0400

    Changes to background pic and added slogan to page Can't get the text to center on the page

--- src/components/Campus/Campus.jsx
+++ src/components/Campus/Campus.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Campus.css'
+import dark_arrow from '../../assets/dark-arrow.png'
 
 const campus = () => {
   return (
-    <div className = 'Campus'>
-      
+    <div className = 'Campus container'>
+      <div className =  "Campus-text">
+        <h1>Your Guide to the Best Bites on University of Toronto Campus </h1>
+        <p>Whether you are a student, faculty, or visitor, you will find a variety of food options on campus. From the best coffee shops to the most affordable meal plans, we have you covered. </p> 
+        <button className = 'btn'>Let's Munch!<img src = {dark_arrow} alt = "" /></button>
+      </div>
     </div>
   )
 }
---
+++ src/components/WhereToEat/WhereToEat.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './WhereToEat.css'
+
+const WhereToEat = () => {
+  return (
+    <div className = 'WhereToEat'>
+        <div className="WhereToEatNext"></div>
+      
+    </div>
+  )
+}
+
+export default WhereToEat
